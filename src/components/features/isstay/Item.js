b5aca6609d98fddfa5159f56c7eaa46c906bdc29
Loi <loidv01071999@gmail.com>
2021-07-12 23:23:03 +0700

    Get API for "Tạm trú tạm vắng" module

--- src/components/features/isstay/Item.js
+++ src/components/features/isstay/Item.js
@@ -1,20 +1,18 @@
 import React from 'react';
-export default function Item(){
-    return(
+export default function Item({ data,index }) {
+    return (
         <>
             <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-
+                <td>{index}</td>
+                <td>{data?.name}</td>
+                <td>{data?.identifyCard}</td>
+                <td>{data?.homeTown}</td>
+                <td>{data?.absentType}</td>
+                <td>{data?.reason}</td>
+                <td>{data?.block}</td>
+                <td>{data?.roomNumber}</td>
+                <td>{data?.startDate}</td>
+                <td>{data?.endDate}</td>
             </tr>
         </>
     )
---
+++ src/lib/DataSource.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:8080/api";
+export const doPost = async (path, headers, formData) => {
+  return axios({
+    url: BASE_URL + path,
+    method: "POST",
+    headers: headers,
+    data: formData,
+  });
+};
+
+export const doGet = async (path, headers) => {
+  return axios({
+    url: BASE_URL + path,
+    method: "GET",
+    headers: headers,
+  });
+};
+
+export const doPut = async (path, headers, formData) => {
+  return axios({
+    url: BASE_URL + path,
+    method: "PUT",
+    headers: headers,
+    data: formData,
+  });
+};
+export const doDelete = async (path) => {
+  return axios({
+    url: BASE_URL + path,
+    method: "DELETE",
+  });
+};
