382447f6283f059c54d2a9d1a52a71f840fe784d
Loi <loidv01071999@gmail.com>
2021-07-12 23:36:34 +0700

    Get API for "Service" module

--- src/components/features/service/ModalDetailService.js
+++ src/components/features/service/ModalDetailService.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
-export default function ModalDetailService({ show, handleClose }) {
+export default function ModalDetailService({ show, handleClose, data }) {
     return (
         <>
             <Modal show={show} onHide={handleClose} animation={false} centered>
@@ -9,38 +9,11 @@ export default function ModalDetailService({ show, handleClose }) {
                     <Modal.Title>Chi tiết dịch vụ</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <ul className="menu">
-                        <li className="menu__item">
-                            <div className="menu__item--title">Tên dịch vụ phụ:</div>
-                            <div className="menu__item--input">
-                                <input type="text" />
-                            </div>
-                        </li>
-                        <li className="menu__item">
-                            <div className="menu__item--title">Tên dịch vụ :</div>
-                            <div className="menu__item--input">
-                                <input type="text" />
-                            </div>
-                        </li>
-                        <li className="menu__item">
-                            <div className="menu__item--title">Chi tiết dịch vụ phụ:</div>
-                            <div className="menu__item--input">
-                                <input type="text" />
-                            </div>
-                        </li>
-                        <li className="menu__item">
-                            <div className="menu__item--title">Lý do:</div>
-                            <div className="menu__item--input">
-                                <input type="text" />
-                            </div>
-                        </li>
-                        <li className="menu__item">
-                            <div className="menu__item--title">Giá:</div>
-                            <div className="menu__item--input">
-                                <input type="text" />
-                            </div>
-                        </li>
-                    </ul>
+                    {data?.map((item, index) => {
+                        return (
+                            <DetailItem data={item} key={index} />
+                        )
+                    })}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
@@ -48,6 +21,56 @@ export default function ModalDetailService({ show, handleClose }) {
                     </Button>
                 </Modal.Footer>
             </Modal>
+        </>
+    )
+}
+
+function DetailItem({ data }) {
+    return (
+        <>
+            <ul className="menu">
+                <li className="menu__item">
+                    <div className="menu__item--title">Tên dịch vụ phụ:</div>
+                    <div className="menu__item--input">
+                        <input type="text"
+                            value={data?.subServiceName}
+                        />
+                    </div>
+                </li>
+                <li className="menu__item">
+                    <div className="menu__item--title">Tên dịch vụ :</div>
+                    <div className="menu__item--input">
+                        <input type="text"
+                            value={data?.serviceName}
+                        />
+                    </div>
+                </li>
+                <li className="menu__item">
+                    <div className="menu__item--title">Chi tiết dịch vụ phụ:</div>
+                    <div className="menu__item--input">
+                        <input type="text"
+                            value={data?.detailSubServiceName}
+                        />
+                    </div>
+                </li>
+                <li className="menu__item">
+                    <div className="menu__item--title">Lý do:</div>
+                    <div className="menu__item--input">
+                        <input type="text"
+                            value={data?.reasonName}
+                        />
+                    </div>
+                </li>
+                <li className="menu__item">
+                    <div className="menu__item--title">Giá:</div>
+                    <div className="menu__item--input">
+                        <input type="text"
+                            value={data?.price}
+                        />
+                    </div>
+                </li>
+            </ul>
+            <hr />
         </>
     )
 }
