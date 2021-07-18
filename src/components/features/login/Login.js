871a45b25a552f9f4386956d1e6e3706580ec5aa
tuanna1301 <59677260+tuanna1301@users.noreply.github.com>
2021-07-18 14:20:19 +0700

    fix error login

--- src/components/features/login/Login.js
+++ src/components/features/login/Login.js
@@ -29,43 +29,43 @@ export default function Login() {
                             <div className="logo text-center"><br />
                                 <a href="#"><p style={{ fontStyle: 'italic', fontSize: '35px', color: 'white' }}><b>AMS Building</b></p></a><br /><br />
                             </div>
-                            <form action="index.html" method="post">
-                                <div className="form-group">
-                                    <label>
-                                        <i className="fas fa-user fa-fw" />
-                                    </label>
-                                    <input type="text" className="form-control" placeholder="Username" name="username" required onChange={(e) => setAccount({
-                                        ...account,
-                                        username: e.target.value
-                                    })} />
+
+                            <div className="form-group">
+                                <label>
+                                    <i className="fas fa-user fa-fw" />
+                                </label>
+                                <input type="text" className="form-control" placeholder="Username" name="username" required onChange={(e) => setAccount({
+                                    ...account,
+                                    username: e.target.value
+                                })} />
+                            </div>
+                            <div className="form-group">
+                                <label>
+                                    <i className="fas fa-key fa-fw" />
+                                </label>
+                                <input type="password" className="form-control" placeholder="Password" name="password" required onChange={(e) => setAccount({
+                                    ...account,
+                                    password: e.target.value
+                                })} />
+                            </div>
+                            <br />
+                            <div className="form-group">
+                                <div className="col">
+                                    <button className="btn btn-sm" onClick={() => login()}>
+                                        <i className="fas fa-sign-in-alt fa-fw mr-1" />Login
+                                    </button>
                                 </div>
-                                <div className="form-group">
-                                    <label>
-                                        <i className="fas fa-key fa-fw" />
-                                    </label>
-                                    <input type="password" className="form-control" placeholder="Password" name="password" required onChange={(e) => setAccount({
-                                        ...account,
-                                        password: e.target.value
-                                    })} />
+                            </div>
+                            <div className="form-group">
+                                <div className="col">
+                                    <a className="link-effect" href="#">
+                                    </a>
+                                    <a className="link-effect float-right" href="forgotPassword.html">
+                                        <i className="fas fa-question fa-fw mr-1" />Forgot password
+                                    </a><br /><br />
                                 </div>
-                                <br />
-                                <div className="form-group">
-                                    <div className="col">
-                                        <button className="btn btn-sm" onClick={() => login()}>
-                                            <i className="fas fa-sign-in-alt fa-fw mr-1" />Login
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="col">
-                                        <a className="link-effect" href="#">
-                                        </a>
-                                        <a className="link-effect float-right" href="forgotPassword.html">
-                                            <i className="fas fa-question fa-fw mr-1" />Forgot password
-                                        </a><br /><br />
-                                    </div>
-                                </div>
-                            </form>
+                            </div>
+
                         </div>
                     </div>
                 </div>
