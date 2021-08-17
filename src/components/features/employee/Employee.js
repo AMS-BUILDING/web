9a731cff199f698a2a5ae47cbe61345a8edb9656
tuanna1301 <59677260+tuanna1301@users.noreply.github.com>
2021-08-17 22:38:07 +0700

    upđate ui screen

--- src/components/features/employee/Employee.js
+++ src/components/features/employee/Employee.js
@@ -50,94 +50,91 @@ export default function Employee() {
         setPositionId(id)
     }
     return <>
-        <div className="breadcrumbs">
-            <div className="col-sm-4">
-                <div className="page-header float-left">
-                    <div className="page-title">
-                        <h1>Nhân viên</h1>
+        <div className="container-fluid">
+            <h4 style={{ marginTop: 20, marginBottom: 20 }}>Nhân viên</h4>
+            <div className={style.wrapContent}>
+                <div className={style.wrapSearch}>
+                    <input
+                        placeholder="Tên nhân viên"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        className={style.ipSearch}
+                    />
+                    <input
+                        placeholder="Số điện thoại"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                        className={style.ipSearch}
+                    />
+                    <input
+                        placeholder="Số CCCD"
+                        value={identifyCard}
+                        onChange={e => setIdentifyCard(e.target.value)}
+                        className={style.ipSearch}
+                    />
+                    <Search positionId={positionId} handlePositionId={handlePositionId} />
+                    <button onClick={() => {
+                        search()
+                        setActivePage(1)
+                    }} className={style.btnSearch}><svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                        </svg></button>
+                    <div>
+                        <button className={style.btnAdd} onClick={handleShow}>Thêm Nhân Viên</button>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div className={style.wrapSearch}>
-            <input
-                placeholder="Tên nhân viên"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                className={style.ipSearch}
-            />
-            <input
-                placeholder="Số điện thoại"
-                value={phone}
-                onChange={e => setPhone(e.target.value)}
-                className={style.ipSearch}
-            />
-            <input
-                placeholder="Số CCCD"
-                value={identifyCard}
-                onChange={e => setIdentifyCard(e.target.value)}
-                className={style.ipSearch}
-            />
-            <Search positionId={positionId} handlePositionId={handlePositionId} />
-            <button onClick={() => {
-                search()
-                setActivePage(1)
-            }} className={style.btnSearch}><svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
-                </svg></button>
-            <div>
-                <button className={style.btnAdd} onClick={handleShow}>Thêm Nhân Viên</button>
-            </div>
-        </div>
+                <div className="main__table">
+                    <table>
+                        <tr>
+                            <th>STT</th>
+                            <th>Tên</th>
+                            <th>Giới tính</th>
+                            <th>Số điện thoại</th>
+                            <th>Địa chỉ</th>
+                            <th>Email</th>
+                            <th>Vị trí</th>
+                            <th>Chức năng</th>
 
-        <div className="main__table">
-            <table>
-                <tr>
-                    <th>STT</th>
-                    <th>Tên</th>
-                    <th>Giới tính</th>
-                    <th>Số điện thoại</th>
-                    <th>Địa chỉ</th>
-                    <th>Email</th>
-                    <th>Vị trí</th>
-                    <th>Chức năng</th>
-
-                </tr>
+                        </tr>
+                        {data?.totalElement > 0 ?
+                            data?.data?.map((item, idx) => {
+                                return (
+                                    <Item data={item} key={idx} index={parseInt(5 * (activePage - 1) + idx + 1)} search={search} />
+                                )
+                            }) :
+                            <>
+                                <tbody >
+                                    <tr >
+                                        <td colSpan="8">Không có dữ liệu</td>
+                                    </tr>
+                                </tbody>
+                            </>
+                        }
+                    </table>
+                </div>
                 {data?.totalElement > 0 ?
-                    data?.data?.map((item, idx) => {
-                        return (
-                            <Item data={item} key={idx} index={parseInt(5 * (activePage - 1) + idx + 1)} search={search} />
-                        )
-                    }) :
-                    <>
-                        <tbody >
-                            <tr >
-                                <td colSpan="8">Không có dữ liệu</td>
-                            </tr>
-                        </tbody>
-                    </>
+                    <div className="wrapper-paginate">
+                        <Pagination
+                            activePage={activePage}
+                            itemsCountPerPage={5}
+                            totalItemsCount={parseInt(data?.totalElement)}
+                            pageRangeDisplayed={3}
+                            onChange={(item) => setActivePage(item)}
+                        />
+                    </div> : <></>
                 }
-            </table>
-        </div>
-        {data?.totalElement > 0 ?
-            <div className="wrapper-paginate">
-                <Pagination
-                    activePage={activePage}
-                    itemsCountPerPage={5}
-                    totalItemsCount={parseInt(data?.totalElement)}
-                    pageRangeDisplayed={3}
-                    onChange={(item) => setActivePage(item)}
-                />
-            </div> : <></>
-        }
-        <ModalMessage message={message} showMessage={showMessage} handleCloseMessage={handleCloseMessage} search={search} />
+                <ModalMessage message={message} showMessage={showMessage} handleCloseMessage={handleCloseMessage} search={search} />
 
-        <ModalAdd
-            show={show}
-            handleClose={handleClose}
-            handleShow={handleShow}
-            search={search}
-        />
+                <ModalAdd
+                    show={show}
+                    handleClose={handleClose}
+                    handleShow={handleShow}
+                    search={search}
+                />
+            </div>
+        </div>
+
+
     </>
 }
--- src/components/features/resident/Resident.js
+++ src/components/features/resident/Resident.js
@@ -36,86 +36,81 @@ export default function Resident({ roomName, handleRoomName }) {
         }
     }
     return <>
-        <div className="breadcrumbs">
-            <div className="col-sm-4">
-                <div className="page-header float-left">
-                    <div className="page-title">
-                        <h1>Cư dân</h1>
-                    </div>
+        <div className="container-fluid">
+            <h4 style={{ marginTop: 20, marginBottom: 20 }}>Cư dân</h4>
+            <div className={styles.wrapContent}>
+                <div className={styles.wrapSearch}>
+                    <input
+                        placeholder="Tên"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        className={styles.ipSearch}
+                    />
+                    <input
+                        placeholder="Số phòng"
+                        value={roomName}
+                        onChange={e => handleRoomName(e.target.value)}
+                        className={styles.ipSearch}
+                    />
+                    <input
+                        placeholder="Số điện thoại"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                        className={styles.ipSearch}
+                    />
+                    <button
+                        onClick={() => {
+                            search()
+                            setActivePage(1)
+                        }}
+                        className={styles.btnSearch}><svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                        </svg></button>
+                    {apartmentId && <button className={styles.btnAdd} onClick={() => setShow(true)}>Thêm cư dân</button>}
+                    <ModalAdd show={show} handleShow={handleShow} handleClose={handleClose} apartmentId={apartmentId} search={search} />
                 </div>
-            </div>
 
+                <div className="main__table">
+                    <table>
+                        <tr>
+                            <th>STT</th>
+                            <th>Tên</th>
+                            <th>Số điện thoại</th>
+                            <th>Số phòng</th>
+                            <th>Chức năng</th>
 
-        </div>
-
-        <div className={styles.wrapSearch}>
-            <input
-                placeholder="Tên"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                className={styles.ipSearch}
-            />
-            <input
-                placeholder="Số phòng"
-                value={roomName}
-                onChange={e => handleRoomName(e.target.value)}
-                className={styles.ipSearch}
-            />
-            <input
-                placeholder="Số điện thoại"
-                value={phone}
-                onChange={e => setPhone(e.target.value)}
-                className={styles.ipSearch}
-            />
-            <button
-                onClick={() => {
-                    search()
-                    setActivePage(1)
-                }}
-                className={styles.btnSearch}><svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
-                </svg></button>
-            {apartmentId && <button className={styles.btnAdd} onClick={() => setShow(true)}>Thêm cư dân</button>}
-            <ModalAdd show={show} handleShow={handleShow} handleClose={handleClose} apartmentId={apartmentId} search={search} />
-        </div>
-
-        <div className="main__table">
-            <table>
-                <tr>
-                    <th>STT</th>
-                    <th>Tên</th>
-                    <th>Số điện thoại</th>
-                    <th>Số phòng</th>
-                    <th>Chức năng</th>
-
-                </tr>
+                        </tr>
+                        {data?.totalElement > 0 ?
+                            data?.data?.map((item, idx) => {
+                                return (
+                                    <Item data={item} key={idx} index={parseInt(5 * (activePage - 1) + idx + 1)} search={search} />
+                                )
+                            }) :
+                            <>
+                                <tbody >
+                                    <tr >
+                                        <td colSpan="5">Không có dữ liệu</td>
+                                    </tr>
+                                </tbody>
+                            </>
+                        }
+                    </table>
+                </div>
                 {data?.totalElement > 0 ?
-                    data?.data?.map((item, idx) => {
-                        return (
-                            <Item data={item} key={idx} index={parseInt(5 * (activePage - 1) + idx + 1)} search={search} />
-                        )
-                    }) :
-                    <>
-                        <tbody >
-                            <tr >
-                                <td colSpan="5">Không có dữ liệu</td>
-                            </tr>
-                        </tbody>
-                    </>
+                    <div className="wrapper-paginate">
+                        <Pagination
+                            activePage={activePage}
+                            itemsCountPerPage={5}
+                            totalItemsCount={parseInt(data?.totalElement)}
+                            pageRangeDisplayed={3}
+                            onChange={(item) => setActivePage(item)}
+                        />
+                    </div> : <></>
                 }
-            </table>
+            </div>
         </div>
-        {data?.totalElement > 0 ?
-            <div className="wrapper-paginate">
-                <Pagination
-                    activePage={activePage}
-                    itemsCountPerPage={5}
-                    totalItemsCount={parseInt(data?.totalElement)}
-                    pageRangeDisplayed={3}
-                    onChange={(item) => setActivePage(item)}
-                />
-            </div> : <></>
-        }
+
+
     </>
 
 
