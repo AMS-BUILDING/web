50735962c4cc3725fe70490b0f1df25815eff089
manhnnguyenn <manha2cvp@gmail.com>
2021-08-22 13:51:10 +0700

    manh fix bug

--- src/components/features/home/Home.js
+++ src/components/features/home/Home.js
@@ -7,8 +7,14 @@ import MonthlyVehicle from './MonthlyVehicle';
 import UseRequestService from './UseRequestService';
 export default function Home() {
     return <>
-        <div className="content" style={{ marginLeft: 15 }}>
-            <h4 style={{ marginTop: 20, marginBottom: 20 }}>Dashboard</h4>
+        <div className="breadcrumbs">
+            <div className="col-sm-4">
+                <div className="page-header float-left">
+                    <div className="page-title">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div className="content mt-3">
@@ -18,33 +24,33 @@ export default function Home() {
             <div className="content mt-3">
                 <div className="animated fadeIn">
                     <div className="row">
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }}>
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }}>
                             <TotalRevenue />
                         </div>
-                        <div className="col" style={{ marginBottom: 30 }} />
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }}>
+                        <div className="col-lg-1" style={{ marginBottom: 30 }} />
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }}>
                             <CountRequestService />
                         </div>
-
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }}>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }}>
                             <MonthlyAccount />
                         </div>
-                        <div className="col" style={{ marginBottom: 30 }} />
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }}>
+                        <div className="col-lg-1" style={{ marginBottom: 30 }} />
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }}>
                             <TypeApartmentAccount />
                         </div>
-
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }}>
-                            <MonthlyVehicle />
-
-                        </div>
-                        <div className="col" style={{ marginBottom: 30 }} />
-                        <div className="col-lg-5" style={{ marginBottom: 30, backgroundColor: 'white' }} >
-                            <UseRequestService />
-                        </div>
-
                     </div>
 
+                    <div className="row">
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }}>
+                            <MonthlyVehicle />
+                        </div>
+                        <div className="col-lg-1" style={{ marginBottom: 30 }} />
+                        <div className="col-lg" style={{ marginBottom: 30, backgroundColor: "white" }} >
+                            <UseRequestService />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
